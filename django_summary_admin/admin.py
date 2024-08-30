a3bbf22d495e70bdb4f0242737f75c096f1d6c40
Eduardo Stefanello <eduardo@stefanello.cc>
2024-08-29 22:41:22 -0300

    Change Links

--- django_summary_admin/admin.py
+++ django_summary_admin/admin.py
@@ -16,3 +16,22 @@ class SummaryAdmin(admin.ModelAdmin):
         except AttributeError:
             pass
         return view
+
+
+class LinksAdmin(admin.ModelAdmin):
+    change_form_template = 'change_links/change_form.html'
+
+    def get_links(self, obj):
+        return None
+
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        view = super().change_view(request, object_id, form_url, extra_context)
+        if not object_id:
+            return view
+        try:
+            view.context_data['links'] = self.get_links(view.context_data['original'])
+        except KeyError:
+            pass
+        except AttributeError:
+            pass
+        return view
--- django_summary_admin/apps.py
+++ django_summary_admin/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class DjangoSummaryAdminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'django_summary_admin'
